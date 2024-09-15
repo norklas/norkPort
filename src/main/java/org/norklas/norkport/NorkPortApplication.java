68e293793bb50895f9d3ffbf076aca8d9c5f4263
norklas <nickm890@gmail.com>
2024-09-15 11:55:23 -0400

    initial commit

---
+++ src/main/java/org/norklas/norkport/NorkPortApplication.java
@@ -0,0 +1,13 @@
+package org.norklas.norkport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NorkPortApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NorkPortApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/norklas/norkport/NorkPortApplicationTests.java
@@ -0,0 +1,13 @@
+package org.norklas.norkport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NorkPortApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
